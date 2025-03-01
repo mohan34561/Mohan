5db4db60042865548d7fbc4951f2148c12daf4d3
mohan34561 <mmohanaselvan2006@gmail.com>
2025-03-01 10:29:59 +0530

    'login'

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+.background{
+    background-image: linear-gradient(rgb(22, 22, 22), rgb(44, 44, 44),rgb(22, 22, 22));
+height: 100vh;
+    width: 100vw;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.box{
+width:  15%;
+border: 3px solid rgb(54, 53, 53);                     
+padding: 70px;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+border-radius: 15px;
+background-image: linear-gradient(rgb(0, 0, 0),rgb(32, 32, 32),rgb(3, 3, 3))
+;
+
+}
+.image{
+    margin-bottom: 30px;
+display: flex;
+height: 100px;
+width: 100px;
+justify-content: center;
+}
+.bt{
+    display: flex;
+    margin-top: 20px;
+    color: black;
+    border: 1px solid red;
+}
+.pt{
+    color: red;}
+.jt{
+    color: red;
+}
